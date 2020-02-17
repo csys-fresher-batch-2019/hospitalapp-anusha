c00daa47ad0539992326c6ef6ed9d142a5be05a8
Anusha S <anusha.gsa@gmail.com>
2020-02-17 09:49:46 +0530

    update

--- src/main/java/com/hospital/manage/ConnectionUtil.java
+++ src/main/java/com/hospital/manage/ConnectionUtil.java
@@ -4,6 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.sqlobject.SqlObjectPlugin;
+
 public class ConnectionUtil {
 	
 	
@@ -17,6 +20,22 @@ public class ConnectionUtil {
 	Class.forName("oracle.jdbc.driver.OracleDriver");
 	return (DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "system","oracle"));
 	
+	}
+	
+	public static Jdbi getJdbi() {
+		Connection connection = null;
+		try {
+			connection = ConnectionUtil.getconnection();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Jdbi jdbi = Jdbi.create(connection);
+		jdbi.installPlugin(new SqlObjectPlugin());
+		return jdbi;
 	}
 
 }
